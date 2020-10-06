a64ecb562f43637da88468aaa43bb0eabb6b3282
MilagrosGaundri <miligaundri10@gmail.com>
2020-10-06 16:48:14 -0300

    agregar imagenes de productos funcionando

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController')
+const  upImagesProducts =require('../middlewares/upImagesProducts')
 
 router.get('/',productsController.listar);
 router.get('/detalle/:id',productsController.detalle);
@@ -9,7 +10,7 @@ router.get('/search',productsController.search);
 // no pasa nada que sea la misma ruta ya que no van por el mismo metodo
 router.get('/add', productsController.agregar)
 router.get('/add/form', productsController.agregar)
-router.post('/add/form', productsController.publicar)
+router.post('/add/form',upImagesProducts.any(), productsController.publicar)
 
 
 module.exports= router;
